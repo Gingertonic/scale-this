eff661452a907fe68cbf1c0dca69959991c06a98
Gingertonic <bethmschofield@gmail.com>
2018-07-07 19:04:40 +1000

    Add change root note feature

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -4,9 +4,14 @@ class ScalesController < ApplicationController
   end
 
   def show
-    # raise params.inspect
     @scale = Scale.find_by(name: params[:scale_slug])
     @midi_notes = @scale.midi_generator(params[:root_note].to_i, 1)
     @notes = @scale.see_notes(params[:root_note].to_i, 1)
+    @roots = Note.all
   end
+
+  def change_root
+    redirect_to scale_path({scale_slug: params[:scale_slug], root_note: params[:root]})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :scales, only: [:index]
-get '/scales/:scale_slug/:root_note', to: 'scales#show'
+get '/scales/:scale_slug/:root_note', to: 'scales#show', as: 'scale'
+post '/scales/:scale_slug', to: 'scales#change_root'
 
 resources :users, only: [:new]
 
