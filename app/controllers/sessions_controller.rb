b3cc983faa9cb22560b80003981cb9a99d94b925
Gingertonic <bethmschofield@gmail.com>
2018-07-07 21:34:31 +1000

    Adjust semnatics to musician

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = Musician.find_by(email: params[:email])
-    (session[:user_id] = user.id) if (user && user.authenticate(params[:password]))
+    if request.env
+      user = Musician.from_omniauth(auth)
+      session[:user_id] = user.id
+    else
+      user = Musician.find_by(email: params[:email])
+      (session[:user_id] = user.id) if (user && user.authenticate(params[:password]))
+    end
     redirect_to scales_path
   end
 
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  private
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -3,6 +3,15 @@ class Musician < ApplicationRecord
   has_many :practises
   has_many :scales, through: :practises
 
+  def self.from_omniauth(auth)
+    where(uid: auth['uid']).first_or_create do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+      u.provider = auth['provider']
+    end
+  end
+
   def self.find_by_slug(slug)
     #find user with name "Al Gakovic" from 'al-gakovic'
     name = slug.split("-").join(" ").titleize
