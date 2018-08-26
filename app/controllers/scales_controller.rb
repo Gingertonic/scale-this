7029744fc04fe811d5d4d66b0c009abe6e2102f5
Gingertonic <bethmschofield@gmail.com>
2018-08-26 11:08:52 +1000

    Update layout and add basic dom manipulation on scales index sidebar

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -2,6 +2,9 @@ class ScalesController < ApplicationController
 
   def index
     @scales = Scale.custom_index(current_user)
+    @scale = Scale.new
+      @note_selection = Note.references
+      @pattern = []
     @types = Scale.types #- MOVE TO JS CLASS OBJECT
     respond_to do |f|
       f.html { render 'index' }
