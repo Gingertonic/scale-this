3a0f50862ca9c28f73ae7eb00667597aa9c68112
Gingertonic <bethmschofield@gmail.com>
2018-09-04 14:41:38 +1000

    tidy models

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -56,51 +56,8 @@ class Scale < ApplicationRecord
     end
   end
 
-  def scale_generator(root, octaves) #returns the frequencies of a scale pattern calculated from the given root note
-    frequencies(root, octaves)
-  end
-
-  def see_notes(root, octaves) #returns the notes as a scale pattern calculated from the root note
-    notes = []
-    midi_generator(root, octaves).each do |midi_value|
-      notes << Note.find_by(midi_value: midi_value).solfege[0...-1]
-    end
-    notes
-  end
-
-  def frequencies(root, octaves) #helper method for scale_generator
-    frequencies = []
-    midi_generator(root, octaves).each do |midi_value|
-      frequencies << Note.find_by(midi_value: midi_value).frequency
-    end
-    frequencies
-  end
-
-  def midi_generator(root, octaves) #returns the midi values of a scale calculated from the root note
-    degrees = []
-    pattern.split("").each do |st_count|
-      degrees << st_count.to_i
-    end
-    midi_array = [root]
-    degrees.each do |st_count|
-      midi_array << midi_array.last + st_count
-    end
-    if octaves == 2
-      octave_two = []
-      midi_array.each do |note|
-        octave_two << note + 12
-      end
-      midi_array.push(octave_two).flatten!.uniq!
-    end
-    midi_array
-  end
-
   def slugify #eg. make 'f-sharp' from 'f#'
     name.gsub(" ", "-").gsub("#","sharp")
-  end
-
-  def not_your_scale(current_user) #checks if the scale is available to the user
-    private && created_by != current_user.id
   end
 
 end
